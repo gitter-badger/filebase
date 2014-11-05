6ee9f160cad7c7e909216d6bf619e88ec16a38e4
omeid <public@omeid.me>
2014-11-05 07:11:09 -0500

    Add example stub and todo item.

---
+++ example/main.go
@@ -0,0 +1,16 @@
+package main
+
+type Article struct {
+	Title   string
+	Content string
+}
+
+type Comment struct {
+	Article int
+	Name    string
+	Content string
+}
+
+func main() {
+	//TODO: Write a simple blog here.
+}
